be62a05d05d17d2c3b38d66825df03594fb8e346
Dhruv Lalwani <dhruvlalwani004@gmail.com>
2024-03-17 04:16:20 +0530

    folder resturcturing

---
+++ routes/category/subcat.js
@@ -0,0 +1,55 @@
+const express =require('express');
+const router = express.Router();
+const SubCategory = require('../../models/category/subcat');
+
+router.get('/',async (req,res)=> {
+    try{
+        const subcategory = await SubCategory.find();
+        res.status(201).json(subcategory);
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
+
+
+
+router.post('/add',async (req,res)=> {
+    try{
+        const {parent,name,status,caticon,bannercolor,fontcolor,leftcolor,rightcolor} = req.body;
+        const cat = await SubCategory.create({
+            Parent:parent,
+            Name:name,
+            Status:status,
+            Icon:caticon,
+            Bannercolor:bannercolor,
+            Headfontcolor:fontcolor,
+            Colorleft:leftcolor,
+            Colorright:rightcolor
+        })
+        const savecat = cat.save()
+        res.status(200).json(savecat);
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+});
+
+router.get('/:id',getcat,(req,res)=> {
+        res.json(res.subcategory);
+})
+async function getcat(req,res,next){
+    try{
+        const subcategory = await SubCategory.findById(req.params.id);
+        if(!subcategory){
+            return res.status(404).json({message:"Not Found"})
+        }
+        res.subcategory = subcategory;
+    }catch(error){
+        return res.status(500).json({message:error.message})
+    }
+}
+
+
+
+
+
+module.exports = router;
---
+++ routes/pagestyles/pages.js
@@ -0,0 +1,41 @@
+const express =require('express');
+const router = express.Router();
+const Temple = require('../../models/pages/temples');
+const Aarti = require('../../models/pages/aarti');
+const Extrapg = require('../../models/pages/extra');
+const Scriptures = require('../../models/pages/scriptures');
+
+
+router.get('/',async (req,res)=> {
+    try{
+
+        const Scripture = await Scriptures.find();
+        const Temples = await Temple.find();
+        const Aartis = await Aarti.find();
+        const Extrapgs = await Extrapg.find();
+        let  data = [...Scripture,...Temples,...Aartis,...Extrapgs];
+        data.sort((Scripture,Temples,Aartis,Extrapgs)=>Scripture-Temples-Aartis-Extrapgs);
+        // for (i=0;i<Scripture.length;i++){
+        //     data[i]=Scripture[i];
+        // }
+        // for (i=0;i<Temples.length;i++){
+        //     data[data.length+i]=Temples[i];
+        // }
+        // for (i=0;i<Aartis.length;i++){
+        //     data[data.length+i]=Aartis[i];
+        // }
+        // for (i=0;i<Extrapgs.length;i++){
+        //     data[data.length+i]=Extrapgs[i];
+        // }
+        
+        res.status(201).json(data);
+    }catch(error){
+        res.status(500).json({message:error.message});
+    }
+});
+
+
+
+
+
+module.exports = router;
